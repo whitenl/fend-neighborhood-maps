603ae912b2c17c6c80c52ce94fc50a6e1a8803ac
Nic White <nicole.l.white@gmail.com>
2019-02-04 21:26:41 -0800

    Foursquare api setup for location data

--- src/App.js
+++ src/App.js
@@ -7,6 +7,13 @@ import SideBar from './components/SideBar.js';
 // Get your own API key here: https://developers.google.com/maps/documentation/javascript/get-api-key
 const GOOGLE_API_KEY = 'AIzaSyAU-bKT2OZQ1dZs8sSodR9EE1y3pLIgLKA'  
 
+// Foursquare parameters 
+const FS_CLIENT_ID = "GQAHMGBZEJXYAQ3F3XOSN3K2WY5NBF2JLAG00MBIJNGWLF4P";
+const FS_CLIENT_SECRET = "OAC0NNZMJLQQUR1BPGGWZUPXIL13F3ZVBPLHTV2ILIR2V2YQ";
+const FS_QUERY = "coffee";
+const FS_LL = "47.5500832,-122.3177821";
+const FS_V = "20190204";
+
 class App extends Component {
 
   constructor(props) {
@@ -14,10 +21,14 @@ class App extends Component {
     this.state = {
       center: {lat: 47.5500832, lng: -122.3177821},
       zoom: 16,
-  }}
+      venues: [],
+      displayVenues: []
+    }
+  }
 
   componentDidMount() {
-    this.renderMap()
+    this.renderMap();
+    this.getVenues();
   }
 
   renderMap = () => {
@@ -27,13 +38,35 @@ class App extends Component {
 
   // Creates map centered on Georgetown neightborhood in Seattle with zoom set in state
   initMap = () => {
-    var map = new window.google.maps.Map
-    (document.getElementById('map'), 
-    {
-      //center: this.state.center,
-      //zoom: this.state.zoom
-      center: {lat: 47.5500832, lng: -122.3177821},
-      zoom: 16,
+    const map = new window.google.maps.Map(document.getElementById('map'), {
+      center: this.state.center,
+      zoom: this.state.zoom
+      }
+    )
+  }
+
+  getVenues = () => {
+    const endPoint = "https://api.foursquare.com/v2/venues/explore?"
+    const parameters = {
+      client_id: `${FS_CLIENT_ID}`,
+      client_secret: `${FS_CLIENT_SECRET}`,
+      query: `${FS_QUERY}`,
+      ll: `${FS_LL}`,
+      v: `${FS_V}`,
+      radius: 500
+    }
+
+  axios.get(endPoint + new URLSearchParams(parameters))
+    .then(response => {
+      console.log(response.data.response.groups[0].items)
+      this.setState({
+        venues: response.data.response.groups[0].items,
+        displayVenues: response.data.response.groups[0].items,
+      },)
+    })
+    .catch(error => {
+      alert(`ERROR: Unable to fetch data from Foursquare`)
+      console.log("ERROR: " + error)
     })
   }
 
