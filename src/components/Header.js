80c06eb23a603b65ec43ea1361f5232ddf95aeb1
Nic White <nicole.l.white@gmail.com>
2019-02-05 19:49:56 -0800

    code formatting cleanup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,8 @@
-import React, { Component } from 'react';
-
+import React, { Component } from "react";
 
 class Header extends Component {
   render() {
-    return (
-      <h1
-      className="App-header">
-      Coffee in Georgetown
-      </h1>
-    );
+    return <h1 className="App-header">Coffee in Georgetown</h1>;
   }
 }
 
